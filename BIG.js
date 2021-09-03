d5e2d07edd9ebfffea115e9fe8d2721edd2f4e67
doodledink <tomtom272727@aol.com>
2021-09-03 14:57:38 +0100

    whatever

---
+++ BIG.js
@@ -0,0 +1,30 @@
+//temporary
+let num1 = 1;
+
+//constant
+const num2 = 2;
+
+let num3 = 3;
+
+const num4 = 4;
+
+/*
+console.log(num1 + num3);
+
+console.log(num1 - num3);
+
+console.log(num1 * num3);
+
+console.log(num1 / num3);
+*/
+
+let answer = num1 + num2;
+
+console.log(answer);
+
+answer += num4;
+
+console.log(answer);
+
+
+
